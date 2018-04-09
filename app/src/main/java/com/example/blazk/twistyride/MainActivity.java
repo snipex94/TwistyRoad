8669971a6511773dbed0b75c3859aa96a67e9489
snipex94 <blaz.kvas@gmail.com>
2018-04-09 10:53:59 +0200

    - Added maps fragment  - Non working maps menu fragment

--- app/src/main/java/com/example/blazk/twistyride/MainActivity.java
+++ app/src/main/java/com/example/blazk/twistyride/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.blazk.twistyride;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLaunchMaps(View view) {
+        Intent intent = new Intent(this, TrackMapsActivity.class);
+        startActivity(intent);
     }
 }
